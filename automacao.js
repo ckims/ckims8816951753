7668e7ebc757677b498c9c2f3f78759b8aba1984
ckims <italo2.ims@gmail.com>
2017-05-10 18:34:46 -0300

    Create automacao.js

---
+++ automacao.js
@@ -0,0 +1,21 @@
+function AlteraRele1(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "0"){
+        document.getElementById("estado").innerHTML="1";
+       	document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+    } else {
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+    }
+}
+
+function AlteraRele2(){
+    var estado_2 = document.getElementById("estado_2").innerHTML;
+    if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
+    } else {
+        document.getElementById("estado_2").innerHTML="0";
+        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Liga</a>";
+    }
+}
